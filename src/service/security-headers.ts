005df45cc795f1bb3fa1d6f41be013fcfcdececa
grpartichelli <gabriel.partichelli@aegro.com.br>
2023-01-06 11:06:10 -0300

    feat: update headers

--- src/service/security-headers.ts
+++ src/service/security-headers.ts
@@ -4,6 +4,6 @@ import {Injectable} from "@angular/core";
 export class SecurityHeaders {
 
   public get() {
-    return {'userId': 'my_user', 'password': 'test'}
+    return {'username': 'test_user', 'password': 'test_password'}
   }
 }
