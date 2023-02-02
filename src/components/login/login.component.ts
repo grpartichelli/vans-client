297ecaf3f0bbace7fc5b5f05f0965a9a38ec964c
grpartichelli <gabriel.partichelli@aegro.com.br>
2023-02-02 10:53:15 -0300

    feat: add login and registration

---
+++ src/components/login/login.component.ts
@@ -0,0 +1,65 @@
+import {Component, Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {LoginService} from "../../service/login.service";
+import {User} from "../../models/user.model";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+
+@Injectable({providedIn: 'root'})
+export class LoginComponent {
+  public loginValid = true;
+  public registrationValid = true;
+  public isRegistration = false
+  public username = '';
+  public password = '';
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+  ) {}
+
+  public onSubmit(): void {
+    if (this.isRegistration) {
+      this.register();
+    } else {
+      this.login()
+    }
+  }
+
+  public register(): void {
+    this.loginService.register(new User(this.username, this.password))
+      .then(() => {
+        this.registrationValid = true;
+        this.router.navigate(['home']).then();
+      })
+      .catch(() => {
+          this.registrationValid = false;
+        }
+      )
+  }
+
+  public login(): void {
+    this.loginService.login(new User(this.username, this.password))
+      .then(() => {
+        this.router.navigate(['home']).then();
+        this.loginValid = true
+      })
+      .catch(() => {
+          this.loginValid = false
+        }
+      )
+  }
+
+  public setIsRegistration(isRegistration: boolean): void {
+    if (isRegistration) {
+      this.loginValid = true;
+    } else {
+      this.registrationValid = true;
+    }
+    this.isRegistration = isRegistration;
+  }
+}
